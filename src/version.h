d7e560fc5138291342adb20e1098cf6b94db7cc5
Shilyaev Egor <egor.shilyaev@gmail.com>
2014-03-23 00:01:20 +0600

    version: Updated to 0.2.3 (22.03.2014)

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define FILEVER        0,2,2,0
+#define FILEVER        0,2,3,0
 #define PRODUCTVER     FILEVER
-#define STRFILEVER     "0.2.2.0\0"
+#define STRFILEVER     "0.2.3.0\0"
 #define STRPRODUCTVER  STRFILEVER
 #define STRDATE        "22.03.2014\0"
