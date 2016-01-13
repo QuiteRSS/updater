f61d4f0b472754113be197f2c23d4b7b00ffa2f5
Shilyaev Egor <egor.shilyaev@gmail.com>
2016-01-13 23:43:35 +0500

    version: Updated to 0.4.1 (13.01.2016)

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #include "version_rev.h"
 
-#define STRDATE           "09.07.2015\0"
-#define STRPRODUCTVER     "0.4.0\0"
+#define STRDATE           "13.01.2016\0"
+#define STRPRODUCTVER     "0.4.1\0"
 
-#define VERSION           0,4,0
+#define VERSION           0,4,1
 #define PRODUCTVER        VERSION,0
 #define FILEVER           VERSION,VCS_REVISION
 
