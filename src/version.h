a39eb951026e072a6ec5a0e2b8109a9b8a1db436
Shilyaev Egor <egor.shilyaev@gmail.com>
2019-12-22 22:38:42 +0500

    version: Updated to 0.4.5 (22.12.2019)

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #include "version_rev.h"
 
-#define STRDATE           "23.10.2019\0"
-#define STRPRODUCTVER     "0.4.4\0"
+#define STRDATE           "22.12.2019\0"
+#define STRPRODUCTVER     "0.4.5\0"
 
-#define VERSION           0,4,4
+#define VERSION           0,4,5
 #define PRODUCTVER        VERSION,0
 #define FILEVER           VERSION,VCS_REVISION
 
